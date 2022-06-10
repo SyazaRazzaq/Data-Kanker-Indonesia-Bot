9092fdc543b4a3c4caba46ec6e406aa5f91d109e
Asep-Deri <107125509+Asep-Deri@users.noreply.github.com>
2022-06-10 16:42:54 +0700

    Update Quarterly.R

--- Quarterly.R
+++ Quarterly.R
@@ -30,7 +30,7 @@ indikator_token <- create_token(
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
 
 ##Tweet
 gdp_tweet <- paste0("Update GDP Indonesia",
